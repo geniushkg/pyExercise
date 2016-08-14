05c093a38dd3630aab3e06d0738676f17fbe505f
geniushkg <geniushkg@gmail.com>
2016-08-14 23:51:17 +0530

    Strings Sample

---
+++ ex5.py
@@ -0,0 +1,16 @@
+my_name = 'Zed .A .Shaw'
+my_age = 23
+my_height = 75
+my_weight = 170
+my_eyes = 'blue'
+my_teeth = 'white'
+my_hair = 'black'
+print "Let's talk about %s." %my_name
+print "He is %s lbs in weight." %my_weight
+print "He is %s inches tall. "%my_height
+print "He has %s coloured eyes."%my_eyes
+print "He has %s teeth."%my_teeth
+print "He has %s Hair."%my_hair
+
+print "Tricky line given below"
+print "If I add %d,%d and %d  I get %d." %(my_age , my_height , my_weight , my_age + my_height + my_weight)
